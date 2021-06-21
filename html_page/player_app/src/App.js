e562d64de83cf113502bb7d3f7f74d74b53d927d
wiseun <wiseun@wiseunui-MacBookAir.local>
2021-06-21 16:23:58 +0900

    Add homepage code

--- html_page/player_app/src/App.js
+++ html_page/player_app/src/App.js
@@ -1,36 +1,71 @@
 import React from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 /*
 curl -X PUT -d 'isPlaying=false' -d 'currentLink=https://www.youtube.com/watch?v=3iM_06QeZi8' -d 'playList=내 손을 잡아|https://www.youtube.com/watch?v=3iM_06QeZi8|잠 못 드는 밤 비는 내리고|https://www.youtube.com/watch?v=m7mvpe1fVa4' http://127.0.0.1:8000/youtube-remote-player/1/
 */
 
+function PlayList(probs) {
+  const playList = probs.playList;
+  const listItem = playList.map((item) => 
+    <li key = {item.link} > {item.name} </li>
+  );
+
+  return <ul>{listItem}</ul>;
+}
+
 class App extends React.Component {
   state = {
     isLoading: true,
-    playerInfo: []
+    isPlaying: false,
+    currentLink: "https://www.youtube.com/watch?v=3iM_06QeZi8",    
+    playList: []    
   };
-
+/*
   getPlayerInfo = async() => {
     const playerInfo = await axios.get("http://172.30.1.48:8000/youtube-remote-player/1/")
     this.setState({ playerInfo, isLoading: false });
     console.log(playerInfo.data)
   }
-
+*/
   componentDidMount() {
-    this.getPlayerInfo();
+    //this.getPlayerInfo();
+
+    const playerInfo = {
+      data :{
+        isPlaying: false,
+        currentLink: "https://www.youtube.com/watch?v=3iM_06QeZi8",
+        playList: "내 손을 잡아|https://www.youtube.com/watch?v=3iM_06QeZi8|잠 못 드는 밤 비는 내리고|https://www.youtube.com/watch?v=m7mvpe1fVa4"
+      }
+    };
+
+    const parseList = playerInfo.data.playList.split("|");
+    let playList = [];    
+
+    for (let i = 0;i < parseList.length;i+=2) {
+      playList.push({
+        name: parseList[i], 
+        link: parseList[i + 1]
+      });
+    }
+
+    this.setState({      
+      isLoading: false,
+      isPlaying: playerInfo.data.isPlaying,
+      currentLink: playerInfo.data.currentLink,
+      playList: playList
+    });
   }
 
   render() {
-    const { isLoading, playerInfo } = this.state;
+    const { isLoading, playList } = this.state;
+
     return (
       <div>
         { 
           isLoading ? "Loading..." :
-          <div>
-          <p> { playerInfo.data.isPlaying } </p>
-          <p> { playerInfo.data.currentLink } </p>
-          <p> { playerInfo.data.playList } </p>
+          <div>             
+            <PlayList playList={playList} />
           </div>
         } 
       </div>
